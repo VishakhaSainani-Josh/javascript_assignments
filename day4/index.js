5e9a88e155381746fa5fdfc12093d8b1976a58c5
VishakhaSainani <vishakhasainani26@gmail.com>
2025-02-11 18:14:30 +0530

    Add answers

---
+++ day4/index.js
@@ -0,0 +1,36 @@
+/*Create a memoised function that returns the cached value when the same arguments
+are passed. */
+
+const memoize = func => {
+    const cache = {};
+    return (...args) => {
+    console.log(cache)
+       let strKey = args.join(',');
+        if(!cache[strKey]){
+          console.log('adding to cache and fectching');
+          cache[strKey] = func.apply(this, args);
+        } 
+        else{
+            console.log('fetching from cache');
+
+        }
+         return cache[strKey];
+     }
+  }
+  
+  const add = (...args) => {
+    return args.reduce((s, e) => {
+      return s += e;
+    }, 0);
+  }
+  
+  const memoizedAddFunction = memoize(add);
+  
+  memoizedAddFunction(1, 2, 3);
+  memoizedAddFunction(1, 2, 3);
+  memoizedAddFunction(4, 2, 3);
+  memoizedAddFunction(4, 2, 3);
+  memoizedAddFunction(8, 2, 3);
+  memoizedAddFunction(1, 2, 3);
+  memoizedAddFunction(4, 2, 3);
+  memoizedAddFunction(8, 2, 3);
